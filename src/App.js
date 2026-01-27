ea07c08889cd2c7bf8f786755f2e0e02ee5efbb4
gajigaji04 <gajigaji04@naver.com>
2026-01-27 13:01:32 +0900

    로직 수정

--- src/App.js
+++ src/App.js
@@ -1,71 +1,30 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import "./App.css";
 import TodoList from "./components/TodoList";
 import TodoInput from "./components/TodoInput";
 import FilterButtons from "./components/FilterButtons";
+import { useTodos } from "./hooks/useTodos";
 
 function App() {
-  const [todos, setTodos] = useState(() => {
-    const saved = localStorage.getItem("todos");
-    return saved ? JSON.parse(saved) : [];
-  });
+  const { todos, addTodo, editTodo, deleteTodo, toggleTodo } = useTodos();
   const [filter, setFilter] = useState("all");
 
-  //  localStorage 저장
-  useEffect(() => {
-    localStorage.setItem("todos", JSON.stringify(todos));
-  }, [todos]);
-
-  const toggleTodo = (id) => {
-    setTodos(
-      todos.map((todo) =>
-        todo.id === id ? { ...todo, completed: !todo.completed } : todo
-      )
-    );
-  };
-
-  // 글 작성
-  const addTodo = (text) => {
-    setTodos([
-      ...todos,
-      {
-        id: Date.now(),
-        text,
-        completed: false,
-      },
-    ]);
-  };
-
-  // 글 수정
-  const editTodo = (id, newText) => {
-    setTodos(todos.map((t) => (t.id === id ? { ...t, text: newText } : t)));
-  };
-
-  // 글 삭제
-  const deleteTodo = (id) => {
-    setTodos(todos.filter((t) => t.id !== id));
-  };
-
-  // 필터링
   const filteredTodos = todos.filter((t) => {
-    if (filter === "all") return true;
     if (filter === "completed") return t.completed;
     if (filter === "active") return !t.completed;
+    return true;
   });
 
   return (
     <div className="App">
       <h2 className="todo-logo">to-do list</h2>
-
       <FilterButtons filter={filter} setFilter={setFilter} />
-
       <TodoList
         todos={filteredTodos}
         toggleTodo={toggleTodo}
         editTodo={editTodo}
         deleteTodo={deleteTodo}
       />
-
       <TodoInput addTodo={addTodo} />
     </div>
   );
